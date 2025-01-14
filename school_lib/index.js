6ac8c1436a7d9bdce66751e3cbbac03219a41a94
zhangpan <zhangpan.online@outlook.com>
2025-01-14 11:47:29 +0800

    express

--- school_lib/index.js
+++ school_lib/index.js
@@ -1,21 +1,40 @@
-require('./init')
+// require('./init')
 // const sequelize = require('./models/db')
 // const adminServ = require('./services/admin')
 // const bookServ = require('./services/book')
 // const classServ = require('./services/class')
 // const studentServ = require('./services/student')
+console.log('========================')
+console.log('========================')
 
+const express = require('express')
 
+const app = express()
 
-// studentServ.add({name: '张三', birthday: '2000-01-01', mobile: '13345678911', sex: true, ClassId: '16', deletedAt: '2025-01-01'}).then(res => {
-//     console.log(res)
-// }).catch(err => {
-//     console.log(err, 'err')
-// })
-// studentServ.getStudents().then(res => {
-//     console.log(res)
-// })
-// studentServ.delete(502).then(res => {
-//     console.log(res)
-// })
+app.listen(9527, () => {
+  console.log('Server is running on port 9527')
+})
+
+app.get('/abc/:id', (req, res) => {
+    // 请求
+    console.log(req.headers)
+    console.log(req.path)
+    console.log(req.query)
+    console.log(req.params)
+    console.log(res.method)
+    
+    // 响应
+    // res.setHeader('AAA', 'BBB')
+    // res.send(req.query)
+
+    // 重定向
+    // res.status(302).header('location', 'https://www.baidu.com').end()
+    // 等价于
+    // res.status(302).location('https://www.baidu.com').end()
+    // 等价于
+    res.redirect(302, 'https://www.baidu.com')
+
+    
+    console.log('========================', new Date().getSeconds())
+})
 
